05600f3174dda54dc4b2344eb8d3a57761ea6be7
MidhatJalil <MIDHATJALIL345@GMAIL.COM>
2024-08-12 02:07:16 +0500

    Add files via upload

---
+++ Chapter#03/script.js
@@ -0,0 +1,29 @@
+
+    // <!-- Problem #01 -->
+    
+
+        // var age = 28;
+        // alert("I am 28 years old.")
+        
+        // var visitTime = 4;
+        // alert("you have visited this site"+" "+visitTime+" "+"times.");
+
+    //   Problem #02
+    
+    
+          // var birthYear = 1994;
+        // document.write("My birth year is"+" "+birthYear+"<br>" );
+        // document.write("Data type of my declared variable is number")
+
+
+   
+    //  <!-- Problem #03 -->
+
+    
+        var _name = "John Doe";
+        var _title = "T-shirt(s)";
+        var _quantity = 10;
+        document.write("<b>"+_name+"</b>"+" ordered "+"<b>" + _quantity +" " +_title+"</b>"+ " on XYZ Clothing.");
+   
+     
+
